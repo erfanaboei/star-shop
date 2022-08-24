a66193f5530f18272461d6942b16cd0cf64cabec
erfanaboei <erfan.a3840@gmail.com>
2022-08-24 13:16:08 +0430

    Change Color Theme

--- src/components/UI/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/UI/LoadingSpinner/LoadingSpinner.jsx
@@ -7,19 +7,24 @@ function LoadingSpinner() {
   return (
     <>
       <div className={classes.Main}>
-        <Skeleton height="220px" highlightColor="darkgray" />
+        <Skeleton height="220px" baseColor="black" highlightColor="red" />
         <div className={classes.DetailsBox}>
           <div className={classes.TitleBox}>
-            <Skeleton height="30px" highlightColor="darkgray" />
+            <Skeleton height="30px" baseColor="black" highlightColor="red" />
           </div>
           <div className={classes.RatingBox}>
-            <Skeleton height="30px" highlightColor="darkgray" />
+            <Skeleton height="30px" baseColor="black" highlightColor="red" />
           </div>
           <div className={classes.PriceBox}>
-            <Skeleton height="30px" highlightColor="darkgray" />
+            <Skeleton height="30px" baseColor="black" highlightColor="red" />
           </div>
           <div className={classes.ButtonBox}>
-            <Skeleton height="30px" width="80%" highlightColor="darkgray" />
+            <Skeleton
+              height="30px"
+              width="80%"
+              baseColor="black"
+              highlightColor="red"
+            />
           </div>
         </div>
       </div>
